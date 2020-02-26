0aeb14c63d8ad7e71a0187c7b4d69b05f829f29f
fjaschneider <33283311+fjaschneider@users.noreply.github.com>
2020-02-26 16:19:55 -0300

    With PCRaster

--- Script/Run_Lisem.R
+++ Script/Run_Lisem.R
@@ -8,7 +8,7 @@ ui <- fluidPage(actionButton('run','Run executable!'))
 
 server<- function(input,output){
   observeEvent(input$run, {
-    system('cmd.exe', input = 'lisem -b -no -r D:/Modeling/Lisem/Geo_processing/20151224/20151224gps.run')
+    system('cmd.exe', input = 'lisem -b -r D:/Modeling/Lisem/Geo_processing/20151224/20151224gps.run', wait = FALSE)
   })
   }
 
