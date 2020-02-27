43f278275b404c8c6cc6407ca2b6bd6e4306dcb7
fjaschneider <33283311+fjaschneider@users.noreply.github.com>
2020-02-27 13:14:40 -0300

    Simple call!

--- Script/Run_Lisem.R
+++ Script/Run_Lisem.R
@@ -6,11 +6,25 @@ library(shiny)
 
 ui <- fluidPage(actionButton('run_raster', 'PCRaster!'), actionButton('run','LISEM!'))
 
-server<- function(input,output){
+server <- function(input,output){
   observeEvent(input$run, {
-    system('cmd.exe', input = 'lisem -b -r D:/Modeling/Lisem/Geo_processing/20151224/20151224gps.run', wait = FALSE)
-  })
+    system('cmd.exe', input = 'lisem -b -r D:/Modeling/Lisem/Geo_processing/20151224/20151224gps.run', wait = FALSE)})
   }
 
-shinyApp(ui,server)
+server1 <- function(input, output){
+  observeEvent(input$run_raster, {system('cmd.exe', input = 'nutshell')})
+}
 
+shinyApp(ui,server, server1)
+
+
+
+# Call Nutshell
+
+system('cmd.exe', input = 'nutshell')
+
+
+
+# Call openLISEM
+
+system('cmd.exe', input = 'lisem -b -r D:/Modeling/Lisem/Geo_processing/20151224/20151224gps.run')
